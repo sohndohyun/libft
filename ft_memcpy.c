098e833ebecd83533dc8b972104d7ca990fd396f
dsohn <sohndohyun@sondohyeon-ui-Macmini.local>
2020-10-02 02:13:37 +0900

    finderror

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dsohn <dsohn@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/29 13:08:02 by dsohn             #+#    #+#             */
-/*   Updated: 2020/09/30 16:11:36 by dsohn            ###   ########.fr       */
+/*   Updated: 2020/10/02 02:13:10 by dsohn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	const unsigned char		*s = src;
 	unsigned char			*d;
 
-	if (dst == NULL && src == NULL)
+	if (dst == NULL || src == NULL)
 		return (NULL);
 	d = dst;
 	while (n--)
